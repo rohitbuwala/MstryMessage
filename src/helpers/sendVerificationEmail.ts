b1164b2056163f401e4b061430071431ee3e017d
rohit buwala <rohitbuwala821gmail.com>
2026-01-11 16:02:26 +0530

    user authentication with NextAuth and JWT
    - Added sign-in page for user authentication.
    - Created NextAuth options with credentials provider for user login.
    - Implemented user registration API with email verification.
    - Integrated bcrypt for password hashing and verification.
    - Set up database connection using mongoose.
    - Created a context provider for session management.
    - Added middleware for protecting routes based on authentication status.
    - Implemented email sending functionality for verification codes.
    - Defined TypeScript types for API responses and extended NextAuth types.
    - Updated package.json with necessary dependencies for authentication and email handling.

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificatinEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import { ota } from "zod/locales";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+        from: 'you@example.com',
+        to: email,
+        subject: 'MstryMessage | Verification code',
+        react: VerificationEmail({username, otp: verifyCode}),
+});
+        return {success: true, message: "Verication Email send successfully"}
+    } catch (emailerror) {
+        console.error("Error sending verification Email", emailerror)
+        return {success: false, message: "Failed to verification email"}
+    }
+}
