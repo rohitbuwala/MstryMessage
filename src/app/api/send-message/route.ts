5c61c58bd1f04566c9895a66cd491054d61a46c1
rohit buwala <rohitbuwala821gmail.com>
2026-01-13 14:53:49 +0530

    Add POST handler for sending messages with user acceptance check

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,39 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel, { Message } from "@/model/User";
+import { success } from "zod";
+
+
+
+export async function POST(request: Request){
+    await dbConnect()
+
+    const {username, content} = await request.json()
+
+    try {
+        
+        const user = await UserModel.findOne({username})
+        if(!user?.isAcceptingMessage){
+            return Response.json({
+                success: false,
+                messages: "User is Not Accepting the messsage"
+            },
+            {status: 403})
+        }
+
+        const newMessage = {content, createdAt: new Date() }
+        user.message.push(newMessage as Message)
+
+        return Response.json({
+            success: true,
+            message: "Message sent successfully"
+        },
+        {status: 200})
+
+    } catch (error) {
+        console.error("Error adding Message:", error)
+        return Response.json({
+            success: false,
+            message: "internal server error"
+        }, {status: 500})
+    }
+}
