d464be1a12286d8168e345334efa34357a9aed30
rohit buwala <rohitbuwala821gmail.com>
2026-01-17 13:25:13 +0530

    feat: add dashboard and user profile pages, update layout with AuthProvider

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import {  NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt"
-export { default } from "next-auth/middleware"
+//export { default } from "next-auth/middleware"
 
 //this function can be marked `async` if you using  `await` inside
 export async function middleware(request: NextRequest){
